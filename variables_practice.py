66d077580a21d3a728745b8cb6fd840b3cc61993
Brandon.F <90413438+Trav1s44@users.noreply.github.com>
2021-10-28 14:11:50 -0400

    [8B] variable practice

---
+++ variables_practice.py
@@ -0,0 +1,11 @@
+# variable practice, brandon flagg, 10/28/21 1:46pm, variables 0.0
+
+# common Data Types and Variables in python
+
+life_left = 5 # intenger value, all whole numbers, +/- and 0.
+account_balance = 10.50 # floating point value, +/-.
+User_name = "road of age" # string a collection of letters/numbers.
+
+print(life_left)
+print(account_balance)
+print(User_name)
